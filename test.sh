9760c79fb7bcd491504aed7698568d80df63a5c5
azhong <zczwzw@163.com>
2022-11-23 00:35:16 +0800

    modified path

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 
 expr="1+2+3+4*4*89-4*5*6/3"
 
-cmake -B build && cmake --build build
+# cmake -B build && cmake --build build
 
 build/c100 "$expr" >build/tmp.s
-clang build/tmp.s driver.c -o build/cal.out
+clang build/tmp.s src/driver.c -o build/cal.out
 build/cal.out
 
 # Ninja
